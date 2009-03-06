283191a3a0d0ac9910b18cf93b6f1b59479f3270
thomas.desert <thomas.desert@9395427a-052b-11de-b5bf-434079a3519f>
2009-03-06 18:41:10 +0000

    [strace] parsing syscall names

--- trunk/strace/trunk/php/make_syscall_names.php
+++ trunk/strace/trunk/php/make_syscall_names.php
@@ -1,4 +1,38 @@
 #!/usr/local/bin/php
 <?php
+define("SYSCALLS_H", "/usr/include/sys/syscall.h");
+
+
+function getSyscallNames()
+{
+  if (!file_exists(SYSCALLS_H))
+    dieError(SYSCALLS_H.": No such file.");
+
+
+  $strfile = file_get_contents(SYSCALLS_H);
+  $aFile = split("\n", $strfile);
+  $aSyscallNames = array();
+  foreach ($aFile as $line)
+    {
+      if (ereg("#define[[:space:]]*SYS_", $line))
+	{
+	  $newline = ereg_replace("^#define[[:space:]]*SYS_([[:alnum:]_\-]*)[[:space:]]*([0-9]*)$", "\\1:\\2", $line);
+	  if (empty($newline))
+	    dieError("Regexp error while retrieving syscall name: line = [$line]");
+	  $aLine = split(":", $newline);
+	  if (count($aLine) != 2)
+	    dieError("Error while spliting [$newline]");
+	  $aSyscallNames[$aLine[1]] = $aLine[0];
+	}
+    }
+
+  return $aSyscallNames;
+}
+
+function dieError($msg)
+{
+  die("ERROR: $msg\n");
+}
+
 
 ?>
