e599c9b197b692f51924ef2a041e2b4c7928c434
thomas.desert <thomas.desert@9395427a-052b-11de-b5bf-434079a3519f>
2009-03-13 16:36:28 +0000

    D [srace]

---
+++ trunk/strace/trunk/constants.h
@@ -0,0 +1,10 @@
+#ifndef __CONSTANTS__
+# define __CONSTANTS__
+
+# define MY_NAME "Strace"
+# define MY_DESCRIPTION "Tracing syscalls from another process"
+# define VERSION "1.0"
+
+# define MAX_STRING_SIZE 30
+
+#endif
