8a2ed36d37d2e18ced45abc6998c1d31bade92c3
thomas.desert <thomas.desert@9395427a-052b-11de-b5bf-434079a3519f>
2009-03-16 21:54:59 +0000

    [strace]

--- trunk/strace/trunk/readmem.h
+++ trunk/strace/trunk/readmem.h
@@ -1,4 +1,8 @@
 void	*ptrace_read(int p_child, void *child_addr, int size);
 char	*read_string(int p_child, void *child_addr);
+char	read_char(int p_child, void *child_addr);
+int	read_int(int p_child, void *child_addr);
+double	read_double(int p_child, void *child_addr);
+float	read_float(int p_child, void *child_addr);
 void	str_limit(char *str, char *end);
 
